e140b5e23f53efa224a41aedaeab9f9dbf2b499e
Just Chris <chris@latactik.com>
2025-08-30 23:21:58 +0000

    chore: install telescope

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ final class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+    public function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 arch()->preset()->php();
+arch()->preset()->strict();
+arch()->preset()->laravel();
 arch()->preset()->security();
 
 arch()->expect('App')
@@ -13,5 +15,3 @@ arch()->expect('App')
 arch('controllers')
     ->expect('App\Http\Controllers')
     ->not->toBeUsed();
-
-//
